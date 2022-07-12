caded24db711a01341ac8b18945c7543a7c2355d
dj04304 <dj043045@gmail.com>
2022-07-12 20:53:12 +0900

    init commit 220712

---
+++ src/main/java/com/springboot/studyjunhyeong/StudyJunhyeongApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjunhyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJunhyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJunhyeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyjunhyeong/StudyJunhyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjunhyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJunhyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
