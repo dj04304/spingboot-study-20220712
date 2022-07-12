9ef09d3e4ec4c9d6a49c886b7c5d81ca9cfe1990
dj04304 <dj043045@gmail.com>
2022-07-12 21:51:19 +0900

    220712 init commit

--- src/main/java/com/springboot/studyjunhyeong/StudyJunhyeongApplication.java
+++ src/main/java/com/springboot/studyjunhyeong/StudyJunhyeongApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyJunhyeongApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(StudyJunhyeongApplication.class, args);
 	}
